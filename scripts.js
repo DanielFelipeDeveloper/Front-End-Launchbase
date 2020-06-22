b8058cf03137e2aaff0451bead40e9d8356c57c1
DanielFelipeDeveloper <danielfelipedeveloper@gmail.com>
2020-06-22 11:28:08 -0400

    iframe

--- scripts.js
+++ scripts.js
@@ -3,11 +3,14 @@ const cards = document.querySelectorAll('.card')
 
 for (let card of cards) {
     card.addEventListener('click', () => {
+        const videoId = card.getAttribute("id")
         modalOverlay.classList.add('active')
+        modalOverlay.querySelector('iframe').src = `https://www.youtube.com/embed/${videoId}`
     })
 }
 
 document.querySelector('.close-modal').addEventListener('click', () => {
     modalOverlay.classList.remove('active')
+    modalOverlay.querySelector('iframe').src = ""
 })
 
